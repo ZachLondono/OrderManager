f81f40afb19b89362e4683bae61ef4b75776d6cf
ZachLondono <zachary.londono5@gmail.com>
2022-01-13 23:09:49 -0500

    Created request to create company, and controller for company

---
+++ src/OrderManager/Features/Companies/CompanyController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using OrderManager.ApplicationCore.Domain;
+
+namespace OrderManager.ApplicationCore.Features.Companies;
+
+public class CompanyController {
+
+    private readonly ISender _sender;
+
+    public CompanyController(ISender sender) {
+        _sender = sender;
+    }
+
+    public Task<Company> CreateCompany(string name,
+                                        string contactName,
+                                        string contactEmail,
+                                        string contactPhone,
+                                        string addressLine1,
+                                        string addressLine2,
+                                        string addressLine3,
+                                        string city,
+                                        string state,
+                                        string postalCode) {
+        return _sender.Send(new CreateCompany.Command(name, contactName, contactEmail, contactPhone, addressLine1, addressLine2, addressLine3, city, state, postalCode));
+    }
+
+
+}
---
+++ src/OrderManager/Features/Companies/CreateCompany.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using MediatR;
+using OrderManager.ApplicationCore.Domain;
+
+namespace OrderManager.ApplicationCore.Features.Companies;
+
+public class CreateCompany {
+
+    public record Command(
+            string CompanyName,
+            string ContactName,
+            string ContactEmail,
+            string ContactPhone,
+            string AddressLine1,
+            string AddressLine2,
+            string AddressLine3,
+            string City,
+            string State,
+            string PostalCode) : IRequest<Company>;
+
+    public class Validator : AbstractValidator<Command> {
+        public Validator() {
+
+            RuleFor(c => c.CompanyName)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.ContactName).NotNull();
+            RuleFor(c => c.ContactEmail).NotNull();
+            RuleFor(c => c.ContactPhone).NotNull();
+            RuleFor(c => c.AddressLine1).NotNull();
+            RuleFor(c => c.AddressLine2).NotNull();
+            RuleFor(c => c.AddressLine3).NotNull();
+            RuleFor(c => c.City).NotNull();
+            RuleFor(c => c.State).NotNull();
+            RuleFor(c => c.PostalCode).NotNull();
+
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, Company> {
+        
+        public Task<Company> Handle(Command request, CancellationToken cancellationToken) {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
