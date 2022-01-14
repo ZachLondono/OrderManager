877b3d98a193257469aad0f0180da20cc4d3bf97
ZachLondono <zachary.londono5@gmail.com>
2022-01-13 21:47:54 -0500

    Report template stores name and path to template

--- src/OrderManager/Domain/Report.cs
+++ src/OrderManager/Domain/Report.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OrderManager.ApplicationCore.Domain;
+﻿namespace OrderManager.ApplicationCore.Domain;
 
 public class Report {
-
+    
     public int ReportId { get; set; }
-
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public string Template { get; set; } = string.Empty;
 
 }
+
