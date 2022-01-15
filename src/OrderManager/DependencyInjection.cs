ad56b0962187f3acad7ab06c1b4492485f704265
ZachLondono <zachary.londono5@gmail.com>
2022-01-15 00:51:27 -0500

    Added dependency injection

--- src/OrderManager/DependencyInjection.cs
+++ src/OrderManager/DependencyInjection.cs
@@ -4,23 +4,25 @@ using Microsoft.Extensions.DependencyInjection;
 using OrderManager.ApplicationCore.Features.Orders;
 using OrderManager.ApplicationCore.Features.Products;
 using OrderManager.ApplicationCore.Infrastructure;
+using OrderManager.ApplicationCore.Infrastructure.Implementations;
 
 namespace OrderManager.ApplicationCore;
 
 public static class DependencyInjection {
 
-    public static IServiceCollection AddAppplicationCore(this IServiceCollection services) {;
-        //services.AddScoped(typeof(IValidator<Features.Products.Create.Command>), typeof(Features.Products.Create.Validator));
-        //services.AddScoped(typeof(IValidator<Features.Orders.Create.Command>), typeof(Features.Orders.Create.Validator));
+    public static IServiceCollection AddAppplicationCore(this IServiceCollection services) {
+
         services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
-        //services.AddValidatorsFromAssemblyContaining<Features.Products.Create.Validator>();
-        //services.AddValidatorsFromAssemblyContaining<Features.Orders.Create.Validator>();
 
         services.AddMediatR(typeof(DependencyInjection).Assembly);
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipeline<,>));
 
         services.AddTransient<ProductController>();
         services.AddTransient<OrderController>();
+
+        services.AddSingleton<IConfigurationProfile, DBConfigurationProfile>();
+
+        services.AddTransient<DataBaseConfiguration>();
 
         return services;
     }
