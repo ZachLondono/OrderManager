a7aa207ddcc034461120fb1004d7795370a2f35f
ZachLondono <zachary.londono5@gmail.com>
2022-01-16 21:17:38 -0500

    Added id and price to line item

--- src/OrderManager/Domain/LineItem.cs
+++ src/OrderManager/Domain/LineItem.cs
@@ -2,10 +2,14 @@
 
 public class LineItem {
 
+    public int LineItemId { get; set; }
+
     public int Line { get; set; }
 
     public int ProductId { get; set; }
 
-    public IReadOnlyDictionary<string, string>? Attributes { get; set; }
+    public decimal Price { get; set; }
+
+    public IReadOnlyDictionary<string, object>? Attributes { get; set; }
 
 }
