a78e0760f68a6a7c88e92034b2221c782cb81f1b
ZachLondono <zachary.londono5@gmail.com>
2022-01-17 18:33:29 -0500

    Added IConfigutration to ui project

--- src/WinFormsUI/Program.cs
+++ src/WinFormsUI/Program.cs
@@ -3,28 +3,25 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OrderManager.ApplicationCore.Domain;
 using OrderManager.ApplicationCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
 
 namespace OrderManager.WinFormsUI;
 
 internal static class Program {
-
-    private static readonly string _scriptDir = @"C:\Users\Zachary Londono\source\repos\OrderManager\src\WinFormsUI\Scripts\";
-    private static readonly string _connString = "Provider=Microsoft.ace.OLEDB.12.0; Data Source='C:\\Users\\Zachary Londono\\Desktop\\OrderManager.accdb';";
 
     public static IServiceProvider? ServiceProvider { get; private set; }
 
     [STAThread]
     static void Main() {
 
-        AppConfiguration config = new() {
-            ConnectionString = _connString,
-            ScriptDirectory = _scriptDir
-        };
+        IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
 
         ServiceProvider = new ServiceCollection()
             .AddLogging(c => c.AddConsole())
-            .AddAppplicationCore()
-            .AddSingleton(config)
+            .AddAppplicationCore(config)
             .BuildServiceProvider();
 
         ApplicationConfiguration.Initialize();
