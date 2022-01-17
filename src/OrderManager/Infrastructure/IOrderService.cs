3e7f7e3efb34647b94ac6b943360631fa6f8aaae
ZachLondono <zachary.londono5@gmail.com>
2022-01-16 21:16:09 -0500

    started defining order service which will load orders from different online services

---
+++ src/OrderManager/Infrastructure/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using OrderManager.ApplicationCore.Domain;
+
+namespace OrderManager.ApplicationCore.Infrastructure;
+
+public interface IOrderService {
+
+    public IEnumerable<Order> GetNewOrders();
+
+    public Order GetOrder(string refNum);
+
+}
---
+++ src/OrderManager/Infrastructure/Implementations/RichelieuOrderService.cs
@@ -0,0 +1,8 @@
+﻿using OrderManager.ApplicationCore.Domain;
+
+namespace OrderManager.ApplicationCore.Infrastructure.Implementations;
+
+public class RichelieuOrderService : IOrderService {
+    public IEnumerable<Order> GetNewOrders() => throw new NotImplementedException();
+    public Order GetOrder(string refNum) => throw new NotImplementedException();
+}
