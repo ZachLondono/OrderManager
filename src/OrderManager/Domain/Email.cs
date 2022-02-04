14909ad776bb898e3bcec30ad75df68cb24eaab8
ZachLondono <zachary.londono5@gmail.com>
2022-02-03 20:39:19 -0500

    Added fields for email entity and command to create a new email

--- src/OrderManager/Domain/Email.cs
+++ src/OrderManager/Domain/Email.cs
@@ -1,4 +1,22 @@
 ﻿namespace OrderManager.ApplicationCore.Domain;
 
+/// <summary>
+/// Defines the configuration for an email to be sent
+/// </summary>
 public class Email {
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string To { get; set; }
+    
+    public string Cc { get; set; }
+
+    public string Bcc { get; set; }
+
+    public string Subject { get; set; }
+
+    public string Body { get; set; }
+
 }
