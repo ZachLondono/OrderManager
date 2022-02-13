891e71288bd90381108af2958d5166b70150ac70
ZachLondono <zachary.londono5@gmail.com>
2022-02-13 16:29:38 -0500

    Removed temporary model class

---
+++ src/OrderManager/Features/OrderList/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrderManager.Features.OrderList;
+
+public static class DateTimeExtension {
+
+    public static bool IsSameWeek(this DateTime date1, DateTime date2) {
+        var cal = System.Globalization.DateTimeFormatInfo.CurrentInfo.Calendar;
+        var d1 = date1.Date.AddDays(-1 * (int)cal.GetDayOfWeek(date1));
+        var d2 = date2.Date.AddDays(-1 * (int)cal.GetDayOfWeek(date2));
+
+        return d1 == d2;
+    }
+
+}
