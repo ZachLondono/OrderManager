ae33bead87eab1e4fb9ec54c24dc855a50175a98
ZachLondono <zachary.londono5@gmail.com>
2022-02-14 17:16:44 -0500

    Adding some domain unit tests

---
+++ tests/Domain.UnitTests/OrderItemTests.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Domain.UnitTests;
+
+public class OrderItemTests {
+
+    [Fact]
+    public void Should_ContainAttributes() {
+
+        // Arrange
+        List<string> attributes = new() {
+            "AttrA",
+            "AttrB",
+            "AttrC"
+        };
+        CatalogProduct product = new CatalogProduct();
+        product.Name = "Test Product";
+        foreach (var attribute in attributes) {
+            product.AddAttribute(attribute);
+        }
+
+        // Act
+        OrderItem item = new(0, product);
+
+        // Assert
+        foreach (var attribute in attributes) {
+            Assert.True(item.Options.ContainsKey(attribute));
+        }
+
+    }
+
+    [Fact]
+    public void Should_SetOptionValue() {
+        
+        // Arrange
+        string attribute = "AttrA";
+        string val = "ValA";
+        CatalogProduct product = new CatalogProduct();
+        product.AddAttribute(attribute);
+        OrderItem item = new(0, product);
+
+        // Act
+        item.SetOptionValue(attribute, val);
+
+        // Assert
+        Assert.Equal(val, item.Options[attribute]);
+
+    }
+
+    [Fact]
+    public void Should_Not_SetOptionValue() {
+
+        // Arrange
+        string attribute = "AttrA";
+        string val = "ValA";
+        CatalogProduct product = new CatalogProduct();
+        OrderItem item = new(0, product);
+
+        // Act
+        item.SetOptionValue(attribute, val);
+
+        // Assert
+        Assert.Throws<KeyNotFoundException>(() => item.Options[attribute]);
+
+    }
+
+    [Fact]
+    public void Should_Not_SetQuantity() {
+
+        // Arrange
+        CatalogProduct product = new();
+        OrderItem item = new(0, product);
+
+        // Act
+        var exception = Record.Exception(() => item.SetQty(-1));
+
+        // Assert
+        Assert.NotNull(exception);
+
+    }
+
+}
---
+++ tests/Domain.UnitTests/OrderTests.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using System.Linq;
+using Xunit;
+
+namespace Domain.UnitTests;
+
+public class OrderTests {
+
+    [Fact]
+    public void Should_AddItem_ToOrder() {
+
+        // Arrange
+        Order order = new Order();
+        CatalogProduct product = new CatalogProduct();
+        product.Name = "Test Product";
+
+        int qty = 1;
+
+        // Act
+        var exception = Record.Exception(() => order.AddItem(product, qty));
+
+        // Assert
+        Assert.Null(exception);
+        Assert.Single(order.Items);
+        Assert.Equal(1, order.Items.Sum(p => p.Qty));
+
+    }
+
+    [Fact]
+    public void Should_NotAddItem_ToOrder() {
+
+        // Arrange
+        Order order = new Order();
+        CatalogProduct product = new CatalogProduct();
+        product.Name = "Test Product";
+
+        int qty = -1;
+
+        // Act
+        var exception = Record.Exception(() => order.AddItem(product, qty));
+
+
+        // Assert
+        Assert.NotNull(exception);
+
+    }
+
+}
