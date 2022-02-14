2fcdcfd6b7ea06e1e54123ecb78afc8eb3467534
ZachLondono <zachary.londono5@gmail.com>
2022-02-14 17:44:49 -0500

    Added parts to products

--- src/Domain/Entities/CatalogProduct.cs
+++ src/Domain/Entities/CatalogProduct.cs
@@ -12,9 +12,22 @@ public class CatalogProduct {
 
     public IReadOnlyCollection<string> Attributes => _attributes.ToList().AsReadOnly();
 
+    private readonly Dictionary<string, Part> _parts = new();
+
+    public IReadOnlyCollection<Part> Parts => _parts.Values.ToList().AsReadOnly();
+
     public void AddAttribute(string attribute) {
+        if (attribute is null)
+            throw new ArgumentNullException(nameof(attribute));
         if (_attributes.Contains(attribute)) return;
         _attributes.Add(attribute);
+    }
+
+    public void AddPart(Part part) {
+        if (part is null)
+            throw new ArgumentNullException(nameof(part));
+        if (_parts.ContainsKey(part.Name)) return;
+        _parts.Add(part.Name, part);
     }
 
 }
