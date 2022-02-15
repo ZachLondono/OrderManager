c143e0db835a8696850cbf5a37717138c0bcda18
ZachLondono <zachary.londono5@gmail.com>
2022-02-14 22:08:08 -0500

    Added persistance for product attributes

---
+++ src/Persistance/Repositories/Catalog/ProductAttribute/IProductAttributeRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Persistance.Repositories.Catalog.ProductAttribute;
+
+public interface IProductAttributeRepository {
+
+    public ProductAttributeDAO CreateAttribute(int productId, string Name);
+
+    public IEnumerable<ProductAttributeDAO> GetAttributesByProductId(int productId);
+
+    public void UpdateAttribute(ProductAttributeDAO productAttribute);
+
+    public void RemoveAttributeFromProduct(int productId, int attributeId);
+
+}
---
+++ src/Persistance/Repositories/Catalog/ProductAttribute/ProductAttributeDAO.cs
@@ -0,0 +1,13 @@
+﻿namespace Persistance.Repositories.Catalog.ProductAttribute;
+
+public class ProductAttributeDAO {
+
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+}
+
+
