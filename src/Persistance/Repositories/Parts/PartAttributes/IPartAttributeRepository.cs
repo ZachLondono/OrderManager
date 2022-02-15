437ee4080c897c912d531b5b1539c81fa0e363ad
ZachLondono <zachary.londono5@gmail.com>
2022-02-14 20:13:30 -0500

    Added repository for part attributes

---
+++ src/Persistance/Repositories/Parts/PartAttributes/IPartAttributeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Persistance.Repositories.Parts.PartAttributes;
+
+public interface IPartAttributeRepository {
+
+    public PartAttributeDAO CreateAttribute(int partId, string Name);
+
+    public void UpdateAttribute(PartAttributeDAO attribute);
+
+    public IEnumerable<PartAttributeDAO> GetAttributesByPartId(int partId);
+
+}
---
+++ src/Persistance/Repositories/Parts/PartAttributes/PartAttributeDAO.cs
@@ -0,0 +1,11 @@
+﻿namespace Persistance.Repositories.Parts.PartAttributes;
+
+public class PartAttributeDAO {
+
+    public int Id { get; set; }
+
+    public int PartId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+}
