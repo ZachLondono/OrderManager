196c5d66d236af7c5bfaa0da7d5e5dd7e39b1d1e
ZachLondono <zachary.londono5@gmail.com>
2022-02-15 20:47:47 -0500

    Renamed file

---
+++ src/OrderManager/Features/OrderDetails/FilledDetails/OrderDetails.cs
@@ -0,0 +1,26 @@
+﻿using OrderManager.Features.OrderDetails.FilledDetails.CompanyDisplay;
+using System;
+
+namespace OrderManager.Features.OrderDetails.FilledDetails;
+
+public class OrderDetails {
+
+    public int Id { get; set; }
+
+    public string Number { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public CompanyDisplayViewModel? Customer { get; set; }
+
+    public CompanyDisplayViewModel? Vendor { get; set; }
+
+    public CompanyDisplayViewModel? Supplier { get; set; }
+
+    public bool IsPriority { get; set; }
+
+    public DateTime LastModified { get; set; }
+
+    public CompanyDisplayViewModel? CompanyModel { get; set; } = new();
+
+}
