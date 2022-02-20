1ea05f4abff53b5ee2720d5c6d44ced31ab01331
ZachLondono <zachary.londono5@gmail.com>
2022-02-20 18:28:56 -0500

    Made program into a singleton class which can be used to get instances of types which are created using the service provider.

--- src/OrderManager/Program.cs
+++ src/OrderManager/Program.cs
@@ -5,24 +5,39 @@ using Avalonia.ReactiveUI;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using Domain;
+using Domain.OrderProvider;
 
 namespace OrderManager;
 
 internal class Program {
 
-    public static IServiceProvider? ServiceProvider { get; private set; }
+    private static Program? _instance = null; 
+
+    internal IServiceProvider ServiceProvider { get; private set; }
+
+    private Program() {
+        ServiceProvider = new ServiceCollection()
+            .AddMediatR(typeof(Program).Assembly)
+            .AddDomain()
+            .AddSingleton<NewOrderProviderFactory>()
+            .BuildServiceProvider();
+    }
+
+    public static T GetInstance<T>() {
+        if (_instance is null) _instance = new Program();
+        return ActivatorUtilities.CreateInstance<T>(_instance.ServiceProvider);
+    }
+
+    public static object GetInstance(Type type) {
+        if (_instance is null) _instance = new Program();
+        return ActivatorUtilities.CreateInstance(_instance.ServiceProvider, type);
+    }
 
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args) {
-
-        ServiceProvider = new ServiceCollection()
-            .AddMediatR(typeof(Program).Assembly)
-            .AddDomain()
-            .BuildServiceProvider();
-
         BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
     }
