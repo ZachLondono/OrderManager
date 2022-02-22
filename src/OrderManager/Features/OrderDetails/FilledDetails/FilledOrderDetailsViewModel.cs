40c1a9e9e7ba402cbc5635fb2f99f3451c360bc0
ZachLondono <zachary.londono5@gmail.com>
2022-02-21 22:31:27 -0500

    Added order remake command and handler

--- src/OrderManager/Features/OrderDetails/FilledDetails/FilledOrderDetailsViewModel.cs
+++ src/OrderManager/Features/OrderDetails/FilledDetails/FilledOrderDetailsViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Entities.OrderAggregate;
+using MediatR;
 using OrderManager.Shared;
 using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive;
+using Unit = System.Reactive.Unit;
 
 namespace OrderManager.Features.OrderDetails.FilledDetails;
 
@@ -13,6 +16,8 @@ public class FilledOrderDetailsViewModel : ViewModelBase {
     public OrderDetails Details { get; set; }
     
     public ReactiveCommand<int, Unit> ReleaseOrder { get; }
+
+    public ReactiveCommand<int, Unit> RemakeOrder { get; }
 
     public FilledOrderDetailsViewModel(Order order) {
         
@@ -45,11 +50,26 @@ public class FilledOrderDetailsViewModel : ViewModelBase {
             },
             Products = productsOrdered
         };
+
         ReleaseOrder = ReactiveCommand.Create<int>(OnOrderRelease);
+        RemakeOrder = ReactiveCommand.Create<int>(OnOrderRemake);
     }
 
     public static void OnOrderRelease(int orderId) {
         Debug.WriteLine($"Releasing order {orderId}");
+    }
+
+    public static async void OnOrderRemake(int orderId) {
+        Debug.WriteLine($"Remaking order {orderId}");
+
+        var sender = Program.GetService<IMediator>();
+        try { 
+            int newId = await sender.Send(new CreateRemake.Command(orderId, new() {
+                new(1, 10)
+            }));
+        } catch(Exception e) {
+            Debug.WriteLine(e);
+        }
     }
 
 }
