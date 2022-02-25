1481df90b72161d80134efaad9bcaab4d9502382
ZachLondono <zachary.londono5@gmail.com>
2022-02-24 21:59:22 -0500

    Making custom title bar

--- src/OrderManager/MainWindow/MainWindow.axaml.cs
+++ src/OrderManager/MainWindow/MainWindow.axaml.cs
@@ -1,17 +1,52 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
-using AvaloniaUI.Ribbon;
+using System.Diagnostics;
+using System.Timers;
 
 namespace OrderManager.MainWindow;
 public partial class MainWindow : Window {
 
+    bool _titlebarSecondClick = false;
+
     public MainWindow() {
         InitializeComponent();
 #if DEBUG
-            this.AttachDevTools();
+        this.AttachDevTools();
 #endif
 
+        this.FindControl<Button>("PART_MinimizeButton").Click += delegate {
+            WindowState = WindowState.Minimized;
+        };
+        this.FindControl<Button>("PART_MaximizeButton").Click += delegate {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        };
+        this.FindControl<Button>("PART_CloseButton").Click += delegate {
+            Close();
+        };
+
+        var titleBar = this.FindControl<Control>("PART_TitleBar");
+        titleBar.PointerPressed += (object? sender, PointerPressedEventArgs ep) => {
+
+            Debug.WriteLine("TitleBar pressed");
+            if (_titlebarSecondClick)
+                WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            else
+                PlatformImpl?.BeginMoveDrag(ep);
+
+
+            if (!_titlebarSecondClick) {
+                _titlebarSecondClick = true;
+
+                Timer secondClickTimer = new Timer(250);
+                secondClickTimer.Elapsed += (sneder, e) => {
+                    _titlebarSecondClick = false;
+                    secondClickTimer.Stop();
+                };
+                secondClickTimer.Start();
+            }
+        };
     }
 
     private void InitializeComponent() {
