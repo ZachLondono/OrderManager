3aac1f3a19416e6274b8451682b1bb9b790a67a1
ZachLondono <zachary.londono5@gmail.com>
2022-04-23 16:33:51 -0400

    Implemented method to parse fields from label file

--- desktop/Infrastructure/Labels/DymoLabelPrinter.cs
+++ desktop/Infrastructure/Labels/DymoLabelPrinter.cs
@@ -1,11 +1,34 @@
 ﻿using OrderManager.ApplicationCore.Labels;
+using System.Xml;
 
 namespace Infrastructure.Labels;
 
 public class DymoLabelPrinter : ILabelPrinter {
+
     public Task<IEnumerable<string>> GetLabelFields(string templatePath) {
-        throw new NotImplementedException();
-    }
+		List<string> fields = new();
+
+		XmlDocument doc = new();
+		doc.Load(templatePath);
+		var labelObjectNodes = doc.SelectNodes("/DieCutLabel/ObjectInfo");
+		if (labelObjectNodes is null)
+			throw new ArgumentException($"The provided file is not a valid label template\n{templatePath}");
+
+		foreach (XmlNode labelObjectInfo in labelObjectNodes) {
+			XmlNodeList childObject = labelObjectInfo.ChildNodes;
+			foreach (XmlNode labelObjectNode in childObject) {
+				if (labelObjectNode.Name.Equals("TextObject") || labelObjectNode.Name.Equals("AddressObject")) {
+					var node = labelObjectNode["Name"];
+					if (node is not null) fields.Add(node.InnerText);
+					break;
+				}
+			}
+		}
+
+		IEnumerable<string> fieldsEnum = fields;
+
+		return Task.FromResult(fieldsEnum);
+	}
 
     public Task PrintLabel(string templatePath, Dictionary<string, string> fields, int qty) {
         throw new NotImplementedException();
