7c3a4ddb9cc2f6528a286766673ecc8d82c1b72b
ZachLondono <zachary.londono5@gmail.com>
2022-05-03 22:30:41 -0400

    Added order DTOs for api

---
+++ desktop/Domain/Orders/OrderDetails.cs
@@ -0,0 +1,29 @@
+﻿namespace OrderManager.Domain.Orders;
+
+public class OrderDetails {
+
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Number { get; set; } = string.Empty;
+
+    public int CustomerId { get; set; }
+
+    public int VendorId { get; set; }
+
+    public int SupplierId { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public DateTime? PlacedDate { get; set; }
+
+    public DateTime? ConfirmedDate { get; set; }
+
+    public DateTime? CompletedDate { get; set; }
+
+    public string Fields { get; set; } = string.Empty;
+
+    public IEnumerable<int> OrderedItems { get; set; } = Enumerable.Empty<int>();
+
+}
---
+++ desktop/Domain/Orders/OrderSummary.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderManager.Domain.Orders;
+
+public class OrderSummary {
+
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Number { get; set; } = string.Empty;
+
+    public int CustomerId { get; set; }
+
+    public DateTime? PlacedDate { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+}
