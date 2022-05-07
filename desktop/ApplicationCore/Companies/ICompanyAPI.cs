f9034176feb265cba4b3c8e86a1d4943f2a5a767
ZachLondono <zachary.londono5@gmail.com>
2022-05-07 18:27:15 -0400

    API interface to interact with backend

---
+++ desktop/ApplicationCore/Companies/ICompanyAPI.cs
@@ -0,0 +1,47 @@
+﻿using OrderManager.Domain.Companies;
+using Refit;
+
+namespace OrderManager.ApplicationCore.Companies;
+
+public interface ICompanyAPI {
+
+    [Post("/Companies")]
+    public Task<int> CreateCompany([Body(buffered: true)] CreateCompanyCommand command);
+
+    public class CreateCompanyCommand {
+        [AliasAs("Name")]
+        public string Name { get; set; } = string.Empty;
+    }
+
+    [Put("/Companies/SetAddress")]
+    public Task SetAddress([Body] SetAddressCommand command);
+
+    public class SetAddressCommand {
+        [AliasAs("CompanyId")]
+        public int CompanyId { get; set; }
+        public string Line1 { get; set; } = string.Empty;
+        public string Line2 { get; set; } = string.Empty;
+        public string Line3 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+    }
+
+    [Put("/Companies/SetName")]
+    public Task SetName([Body] SetNameCommand command);
+
+    public class SetNameCommand {
+        public int CompanyId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    [Get("/Companies/")]
+    public Task<IEnumerable<CompanySummary>> GetCompanies();
+
+    [Get("/Companies/{id}")]
+    public Task<Company> GetCompany(int id);
+
+    [Delete("/Companies/{id}")]
+    public Task RemoveCompany(int id);
+
+}
