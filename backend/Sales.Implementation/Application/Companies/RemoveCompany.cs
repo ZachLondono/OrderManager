ed468ae5845d05a157c2f40f03b2cb23b8f42fbe
ZachLondono <zachary.londono5@gmail.com>
2022-05-11 12:30:38 -0400

    Remove company contacts when the company is removed

--- backend/Sales.Implementation/Application/Companies/RemoveCompany.cs
+++ backend/Sales.Implementation/Application/Companies/RemoveCompany.cs
@@ -11,13 +11,15 @@ public class RemoveCompany {
     public class Handler : AsyncRequestHandler<Command> {
 
         private readonly IDbConnection _connection;
-
+        
         public Handler(IDbConnection connection) {
             _connection = connection;
         }
 
         protected override async Task Handle(Command request, CancellationToken cancellationToken) {
-            const string command = @"DELETE FROM [Sales].[Companies] WHERE [Id] = @Id;";
+
+            const string command = @"DELETE FROM [Sales].[Contacts] WHERE [CompanyId] = @Id;
+                                    DELETE FROM [Sales].[Companies] WHERE [Id] = @Id;";
 
             await _connection.ExecuteAsync(command, new {
                 request.Id
