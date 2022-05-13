dc8dde084e1bfc3629b411cd0e132f96cd9e7148
ZachLondono <zachary.londono5@gmail.com>
2022-05-12 22:19:54 -0400

    Moved EmailAddress model to models namespace

---
+++ desktop/DesktopUI/Models/EmailAddress.cs
@@ -0,0 +1,34 @@
+﻿namespace DesktopUI.Models;
+
+/// <summary>
+/// Keeps track of changes to an email field and keeps the previous value so that it can be overwritten when the new value is saved
+/// </summary>
+public class EmailAddress {
+
+    public bool HasChanged { get; private set; }
+
+    public string? PreviousValue {
+        get;
+        private set;
+    } = string.Empty;
+
+    private string _value;
+    public string Value {
+        get => _value;
+        set {
+            _value = value;
+            HasChanged = true;
+        }
+    }
+
+    public EmailAddress(string initialValue) {
+        _value = initialValue;
+        PreviousValue = initialValue;
+    }
+
+    public void Reset() {
+        HasChanged = false;
+        PreviousValue = Value;
+    }
+
+}
--- desktop/DesktopUI/ViewModels/EmailTemplateEditorViewModel.cs
+++ desktop/DesktopUI/ViewModels/EmailTemplateEditorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using OrderManager.ApplicationCore.Emails;
+﻿using DesktopUI.Models;
+using OrderManager.ApplicationCore.Emails;
 using OrderManager.Domain.Emails;
 using ReactiveUI;
 using System;
@@ -242,39 +243,6 @@ public class EmailTemplateEditorViewModel : ViewModelBase {
             _removedBcc.Add(email.PreviousValue);
         else _removedBcc.Add(email.Value);
         return Unit.Default;
-    }
-
-    /// <summary>
-    /// Keeps track of changes to an email field and keeps the previous value so that it can be overwritten when the new value is saved
-    /// </summary>
-    public class EmailAddress {
-
-        public bool HasChanged { get; private set; }
-
-        public string? PreviousValue {
-            get;
-            private set;
-        } = string.Empty;
-
-        private string _value;
-        public string Value {
-            get => _value;
-            set {
-                _value = value;
-                HasChanged = true;
-            }
-        }
-
-        public EmailAddress(string initialValue) {
-            _value = initialValue;
-            PreviousValue = initialValue;
-        }
-
-        public void Reset() {
-            HasChanged = false;
-            PreviousValue = Value;
-        }
-
     }
 
 }
