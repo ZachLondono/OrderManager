042df90bfed6860134af358c54af08d949fb1472
ZachLondono <zachary.londono5@gmail.com>
2022-05-29 23:47:09 -0400

    Updated SQL to match code

--- backend/Shemas/CreateManufacturingSchema.sql
+++ backend/Shemas/CreateManufacturingSchema.sql
@@ -1,14 +1,23 @@
 create schema Manufacturing;
 Go
+
+create table Manufacturing.WorkCells (
+	[Id] int IDENTITY(1,1) PRIMARY KEY,
+	[Alias] varchar(255),
+	[ProductClass] int
+)
+
 create table Manufacturing.Jobs (
 	[Id] int IDENTITY(1,1) PRIMARY KEY,
 	[Name] varchar(255) NOT NULL,
 	[Number] varchar(255) NOT NULL,
-	[CustomerId] int,
-	[VendorId] int,
-	[ItemCount] int,
+	[CustomerName] varchar(255),
+	[ScheduledDate] datetime,
 	[ReleasedDate] datetime,
 	[CompletedDate] datetime,
 	[ShippedDate] datetime,
-	[Status] varchar(255) NOT NULL
+	[Status] varchar(255) NOT NULL,
+	[ProductClass] int,
+	[ProductQty] int,
+	[WorkCellId] int FOREIGN KEY REFERENCES Manufacturing.WorkCells(Id)
 );
