2a679ce535af995aab648b4063c9e19d4bc45ad4
ZachLondono <zachary.londono5@gmail.com>
2022-06-05 00:17:58 -0400

    Job View Model for JobList View

---
+++ desktop/DesktopUI/Models/Job.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesktopUI.Models;
+
+public class Job {
+
+    public string Number { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Vendor { get; set; } = string.Empty;
+
+    public string Customer { get; set; } = string.Empty;
+
+    public int Qty { get; set; }
+
+    public DateTime? ScheduledDate { get; set; } 
+
+    public string ProductClass { get; set; } = string.Empty;
+
+    public string WorkCell { get; set; } = string.Empty;
+
+}
