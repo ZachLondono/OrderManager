5b4b71836c8f05b68c5a862e2b959eeb98d7af9e
ZachLondono <zachary.londono5@gmail.com>
2022-06-05 00:17:36 -0400

    Switch window view when menu buttons are pressed

--- desktop/DesktopUI/ViewModels/MainWindowViewModel.cs
+++ desktop/DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -15,9 +15,17 @@ enum OrderSort {
     Number = 4
 }
 
+public enum View {
+    OrderList,
+    JobList,
+    Scheduler
+}
+
 public partial class MainWindowViewModel : ViewModelBase {
 
     public RibbonViewModel RibbonDataContext { get; init; }
+    public MonthlyPlannerViewModel schedulerVM { get; } = new();
+    public JobListViewModel jobVM { get; } = new();
 
     public ObservableCollection<OrderListItem> OrderList { get; } = new();
 
@@ -35,6 +43,25 @@ public partial class MainWindowViewModel : ViewModelBase {
         get => _selectedOrderId;
         set => this.RaiseAndSetIfChanged(ref _selectedOrderId, value);
     }
+
+    public ICommand SetOrderListView { get; }
+    public ICommand SetJobListView { get; }
+    public ICommand SetSchedulerView { get; }
+
+    private View _currentView = View.JobList;
+    public View CurrentView {
+        get => _currentView;
+        set {
+            _currentView = value;
+            this.RaisePropertyChanged(nameof(IsOrderListVisible));
+            this.RaisePropertyChanged(nameof(IsJobListVisible));
+            this.RaisePropertyChanged(nameof(IsScheduleVisible));
+        }
+    }
+    
+    public bool IsOrderListVisible => CurrentView == View.OrderList;
+    public bool IsJobListVisible => CurrentView == View.JobList;
+    public bool IsScheduleVisible => CurrentView == View.Scheduler;
 
     public MainWindowViewModel(RibbonViewModel ribbonViewModel) {
         
@@ -94,6 +121,11 @@ public partial class MainWindowViewModel : ViewModelBase {
         OnSelectedOrder = ReactiveCommand.Create<OrderListItem>(o => {
             SelectedOrderId = o.Id;
         });
+
+        SetOrderListView = ReactiveCommand.Create(() => CurrentView = View.OrderList);
+        SetJobListView = ReactiveCommand.Create(() => CurrentView = View.JobList);
+        SetSchedulerView = ReactiveCommand.Create(() => CurrentView = View.Scheduler);
+
 
     }
 
